0de181085395854d9f8e501b8bc638719f781ca9
Devanshukumawat <128327394+Devanshukumawat@users.noreply.github.com>
2023-12-26 13:16:40 +0530

    Add files via upload

---
+++ 12class/js/index.js
@@ -0,0 +1,1030 @@
+const prompt = require("prompt-sync")()
+
+// comment js code
+
+// var let const
+
+/*let firstName = "Devanshu"
+let lastName = "kumawat"
+   lastName = "Sharma"
+console.log("Name "+firstName+"Lastname "+lastName)
+
+const x = 167
+    x = 456
+console.log(x)*/
+
+
+// {
+//     let x = 5
+// }
+
+// console.log(x)
+
+// {
+//     const y = 6
+// }
+
+// console.log(y)
+
+//     var firstName = "Devanshu"
+// }
+// console.log(firstName)
+
+
+
+
+// let  = "devanshu"
+// let Name = "Aman"
+
+// console.log(name)
+// console.log(Name)
+
+// let firstName = "Devansu"
+// console.log(typeof(firstName))
+
+// let age = 51
+// console.log(typeof(age))
+
+// const isMarried = true
+// console.log(typeof(isMarried))
+
+// let a;
+// a = "dev"
+// console.log(typeof(a))
+
+/*let x = 2 + 6 *7
+
+const arry = ["dev",123,"kumawat",145]
+ console.log(arry[2])
+
+const studentName = {
+    name : "Devanshu",
+    class : 12,
+    rollno : 123
+}
+
+console.log("Student Name "+studentName.name+" Class "+studentName.class+" roll-no "+studentName.rollno)*/
+
+
+// let x = 8;
+// let y = 10;
+// let z = x + y
+// console.log(z)
+
+// let a = 100;
+//     a-=5
+// console.log(a)
+
+// let x = 10**3;
+// console.log(x)
+
+// let x = "12"
+// let y = 78
+// let z = x+y
+// console.log(z)
+
+// if , else if , else
+
+// const prompt = require("prompt-sync")()
+
+// let number = prompt("Enter Number-:")
+
+// if(number>0){
+//     console.log("The number is positive")
+// }
+// else if(number==0){
+//     console.log("The Number is zero")
+// }
+// else{
+//     console.log("The number is Nagative either zero")
+// }
+
+
+// const prompt = require("prompt-sync")()
+
+
+// let operator = prompt("Enter Operator (*,+,-,/) ")
+// let number1 = parseInt(prompt("Enter Number-1 ")) 
+// let number2 = parseInt(prompt("Enter Number-2 ")) 
+
+// let result;
+
+// if(operator=="*"){
+//     result= number1*number2
+// }
+// else if(operator=="+"){
+//     result= number1+number2
+// }
+// else if(operator=="-"){
+//     result= number1-number2
+// }
+// else if(operator=="/"){
+//     result= number1/number2
+// }
+// else{
+//     console.log("Invalid Input")
+// }
+
+// console.log(`${number1} ${operator} ${number2} = ${result}`)
+
+// let firstName = Number("Devanshu") 
+// let number = String(98) 
+// console.log(typeof(number))
+// console.log(typeof(firstName))
+
+// const prompt = require("prompt-sync")()
+
+// const num = prompt("Enter Number :- ")
+// if(num % 2 == 0){
+//     console.log("NUmber is even")
+// }
+// else{
+//     console.log("NUmber is odd")
+// }
+
+// const player1 = prompt.hide("Enter Number :- ")
+// const player2 = prompt("Enter Number :- ")
+
+// if(player1 == player2){
+//     console.log("Yaahooo you won..!")
+// }
+// else{
+//     console.log("Better Luck Next time")
+// }
+
+// console.log(`Player1 Number- ${player1} Player2 Number- ${player2}`)
+
+
+// const prompt = require("prompt-sync")()
+
+// const studentNAme = prompt("Enter Student Name :- ")
+// const marks = prompt("Enter Marks :- ")
+// if(marks>=90 && marks<=100){
+//     console.log( "Student Name" +studentNAme+ "A+ Grade")
+// }
+// else if(marks>=80 && marks<90){
+//     console.log( "Student Name" +studentNAme+ "A Grade")
+// }
+// else if(marks>=70 && marks<80){
+//     console.log( "Student Name" +studentNAme+ "B+ Grade")
+// }
+// else if(marks>=60 && marks<70){
+//     console.log( "Student Name" +studentNAme+ "B Grade")
+// }
+// else if(marks>=45 && marks<60){
+//     console.log( "Student Name" +studentNAme+ "C Grade")
+// }
+// else if(marks>=35 && marks<45){
+//     console.log( "Student Name" +studentNAme+ "D Grade")
+// }
+// else{
+//     console.log("Fail phir mile ge...")
+// }
+
+// let fistName = "Devanshu"
+// let lastName ='Hello'
+
+// let text = fistName.length
+// let text1 = lastName.length
+// console.log(text)
+// console.log(text1)
+
+// let text = "Kal  ho na ho"
+
+
+// let text1 = text.slice(4 , 8)
+
+// console.log(text1)
+
+// let text = "     Phir mile ge chal te chal te     "
+// let textTrim = text.trim()
+// let textStrat = text.trimStart()
+// let
+// console.log(text)
+// console.log(textTrim)
+// console.log(textStrat)
+
+// console.log(text.length)
+// console.log(textStrat.length)
+
+
+// let textTrim = text.trim()
+// console.log(textTrim)
+// console.log( "Text Length" +text.length)
+// console.log( "Text TRim Length" +textTrim.length)
+
+// let text = "kal ho na ho ho ho "
+// let textReplace = text.replace("ho","sona")
+// let text1 = text.replaceAll("ho","sona")
+
+// console.log(text)
+// console.log(textReplace)
+// console.log(text1)
+
+
+// let text1 = "Devanshu "
+// let text2 = "KUMAWAT"
+
+// let text3 = text1.concat(text2)
+
+// console.log(text1)
+// console.log(text2)
+// console.log(text3)
+
+
+// let text3 = text1.toUpperCase()
+// let text4 = text2.toLowerCase()
+// console.log(text3)
+// console.log(text4)
+
+// let text = "Kal is baar pkaa hoga"
+
+// let text1 = text.split(" ")
+
+// console.log(typeof(text))
+// console.log(typeof(text1))
+
+// console.log(text1[3])
+
+
+// const arrray = ["devanshu",678,"aman",90,"Abhay",98]
+
+// console.log(arrray)
+// console.log(typeof(arrray))
+// console.log(arrray[3])
+
+// arrray[3] = "Hello"
+// console.log(arrray)
+
+// const Hello = new Array("Devanshu",89,87,"Aman","Swastik")
+// console.log(Hello)
+// console.log(typeof(Hello))
+
+// const myArray = ["DEvanshu",89,"Aman",90,"Swastik"]
+
+// let arrayLeng = myArray.length 
+
+// console.log(arrayLeng)
+
+// let text = myArray.toString()
+// console.log(typeof(text))
+// let text = myArray.pop()
+// console.log(text)
+// console.log(myArray)
+
+// myArray.push("Hello Class")
+
+// console.log(myArray)
+
+// const myArray = ["DEvanshu",89,"Aman",90,"Swastik"]
+
+// let text = myArray.shift()
+// console.log(text)
+// console.log(myArray)
+
+// let text = myArray.join("*")
+
+// console.log(myArray)
+// console.log(text)
+
+// let text = myArray.toString()
+// console.log(myArray)
+// console.log(text)
+
+// myArray[0] = "Devanshu"
+
+// console.log(myArray)
+
+// const myArray = ["DEvanshu",89,"Aman",90,"Swastik"]
+
+//  delete myArray[2]
+
+// console.log(myArray[2])
+
+// const firstarray = ["Devanshu","Swastik","aman","Pushpa","hello","Class"]
+
+// const secondArray = [12,34,56,78,90]
+
+// let newArray= firstarray.concat(secondArray)
+
+// console.log(newArray[4])
+
+// firstarray.splice(1,4)
+
+// console.log(firstarray)
+
+// let newarray = firstarray.slice(4)
+
+// console.log(newarray)
+
+// console.log(firstarray)
+
+// const firstarray = ["Devanshu","Swastik",78,"Aman","Pushpa",67,98]
+
+// let newArray = firstarray.sort()
+
+
+// console.log(firstarray)
+
+
+// let text = "Kal ho na ho "
+
+// let newText = text.includes("z")
+
+// console.log(newText)
+
+// let text = "Kal ho na ho "
+
+// const text2 = text.match("Kal")
+
+// console.log(text2)
+
+
+// let text = "Kal ho na ho "
+ 
+// let text2 = text.charAt(7)
+// console.log(text2)
+
+// let arraay = ["SWastik",98,"Devanshu",96,"Aman","Manish"]
+
+// console.log(arraay)
+
+// for(let x =1; x<=5; x++){
+//     console.log(x)
+// }
+
+
+
+// for(let x = 0; x<arraay.length; x++){
+//     console.log(arraay[x])
+// }
+
+// console.log(arraay[1])
+
+
+// for(let x = 0; x<6; x++){
+//     console.log(arraay[x]+" X Value"+x)
+// }
+
+
+// function fname(){
+//     let x = 4;
+//     let y = 9;
+//     let z = x+y
+//     console.log(z)
+// }
+
+// fname()
+
+// function fname(a,b){
+//     return a*b  
+// }
+// console.log(fname(6,7))
+
+
+
+
+// // function fname(){}
+// let fname= (a,b)=>{
+//     return a+b
+// }
+// console.log(fname(5,6))
+
+
+
+// let arraay = ["SWastik",98,"Devanshu",96,"Aman","Manish"]
+
+// let newArray = arraay.forEach((Value,index) => {
+//    return Value
+// });
+
+// console.log(arraay)
+// console.log(newArray)
+
+// let arraay = ["SWastik",98,"Devanshu",96,"Aman","Manish"]
+
+// let newArray = arraay.map((Value)=>{
+//     return Value
+// })
+
+// console.log(arraay)
+// console.log(newArray)
+
+// const prompt = require("prompt-sync")()
+
+// const userInput = prompt("Enter Number:- ")
+
+// let userInput = 15
+
+// if(userInput<=10){
+//     for(let x =1; x<=userInput; x++){
+//         console.log("hy"+"Number-: "+x)
+//     }
+// }
+// else if(userInput<=20){
+//     for(let x =1; x<=userInput; x++){
+//         console.log("by" +"Number-: "+x)
+//     }
+// }
+
+// let y=10
+
+// for(let x =1; x<=y; x++){
+//     console.log(x)
+// }
+
+
+// const myArray = ["swastik","devanshu","mudit","karan","nilesh"]
+
+
+
+
+// let double= function myfn(value){
+//   return value*2
+// }
+
+// let result = secondArray.map(double)
+
+// console.log(result)
+
+// let result = secondArray.map((value)=>{
+//     return value*2
+// })
+
+// console.log(result)
+
+
+// cp
+
+// const secondArray = [7,9,8,7,6,5,4]
+
+
+// let result = secondArray.filter((value)=>{
+//     return value>=5
+// })
+
+// console.log(result)
+
+
+
+
+
+
+// let isEven = function myfn(value){
+//  return value%2==0
+// }
+// let myfn= (value)=>{
+//     return value%2==0
+// }
+
+// let result = myNum.filter(myfn)
+
+// console.log(result)
+
+
+
+
+
+// let result= myNum.filter((value)=>{
+//     return value %2==0
+// })
+
+// console.log(result)
+
+// const myNum = [6,8,9,6,67,98,55,34,89,100,26,87]
+
+// let result = myNum.reduce((total,value)=>{
+//     return total +value
+// })
+
+// console.log(result)
+
+
+
+/*
+
+user input = ""
+
+
+devanshu = dev
+dev = devanshu
+swastik = satvik
+satvik = swastik
+
+
+*/
+
+
+// const myArray = ['devanshu','swastik','daisy','deep','rajat','aman']
+
+
+//OUTPUT=== ["DEVANSHU","SWASTIK","DAISY","DEEP","RAJAT","AMAN"]
+
+
+// const prompt = require("prompt-sync")()
+
+// const userInput = prompt("Enter Name:- ")
+
+// if(userInput=="devanshu"){
+//     console.log("dev")
+// }
+// else if(userInput== "dev"){
+//     console.log("devanshu")
+// }
+// else if(userInput== "swastik"){
+//     console.log("satvik")
+// }
+// else if(userInput == "satvik"){
+//     console.log("swastik")
+// }
+// else{
+//     console.log("invalid Input")
+// }
+
+// const myArray = ['devanshu','swastik','daisy','deep','rajat','aman']
+
+// let result = myArray.map((value)=>{
+//     return value.toUpperCase()
+// })
+
+// console.log(myArray)
+// console.log(result)
+
+
+// let num = 87
+// let y = "10"-"9"
+// console.log(typeof(y))
+// console.log(typeof(num))
+
+// console.log(y+num)
+// console.log(isNaN(num))
+// console.log(isNaN(y))
+
+
+// let x = parseFloat("77.88")
+ 
+// let y = parseInt("8786")
+
+// console.log(typeof(x))
+
+// console.log(typeof(y))
+
+
+// const prompt = require("prompt-sync")()
+
+
+// let operator = prompt("Enter Operator (*,+,-,/) ")
+// let number1 = parseFloat(prompt("Enter Number-1 ")) 
+// let number2 = parseFloat(prompt("Enter Number-2 "))
+
+// let result;
+
+// if(operator=="*"){
+//     result= number1*number2
+// }
+// else if(operator=="+"){
+//     result= number1+number2
+// }
+// else if(operator=="-"){
+//     result= number1-number2
+// }
+// else if(operator=="/"){
+//     result= number1/number2
+// }
+// else{
+//     console.log("Invalid Input")
+// }
+
+// console.log(`${number1} ${operator} ${number2} = ${result}`)
+
+
+// const myObj = {
+//     name:"Devanshu",
+//     class:6,
+//     age:67,
+//     contact:989878978,
+//     rollno:98,
+//     studentDetails:function add(){
+//         return this.name + " " + this.age + " " + this.class + " " + this.contact
+//     }
+// }
+
+// console.log("Student Name " + myObj.name+" Class "+myObj.class+" Age "+myObj.age+" Contact "+myObj.contact)
+
+// console.log(myObj.studentDetails())
+
+// const student = {
+//     name:"devanshu",
+//     contact:{
+//         email:"devanshu@gmail.com",
+//         Mnum:898989789
+//     },
+//     id:78787,
+//     Address:{
+//         city:"jaipur",
+//         pincode:302020
+//     }
+
+// }
+
+// console.log(student.contact.email)
+// console.log(student.Address.pincode)
+
+// const arry = [
+//     ["Devanshu",98],
+//     ["Swastik",76],
+//     ["aman",76]
+// ]
+
+// for(let x =0; x<arry.length; x++){
+//     for(let y=0; y<arry[x].length; y++){
+//         console.log(arry[x][y])
+//     }
+    
+// }
+
+
+
+
+// console.log(arry[1][0])
+
+// const myObj = {
+//     name:"Devanshu",
+//     class:6,
+//     age:67,
+//     contact:989878978,
+//     rollno:98
+// }
+
+
+
+
+// let age = prompt("Enter age :- ")
+
+// age>=18 ? console.log("valid age to apply license")
+// : console.log("Not valid age to apply license")
+
+// let firstName = prompt("Enter Name:- ")
+// let job = prompt("Enter Input:- ")
+
+// switch(job){
+//     case "teacher":
+//         console.log(firstName+ " teach how to code")
+//         break
+
+//     case "developer":
+//         console.log(firstName+" Creat a website") 
+//         break
+    
+//     case "driver":
+//         console.log(firstName+" Drive a car like ola ")  
+//         break
+        
+//     default:
+//         console.log("Invalid input")    
+
+// }
+
+// let starPrint
+// for(let i = 1; i<)
+
+
+
+
+// let starPrint = 5;
+// let starString = ""
+
+// for(i=1; i<=starPrint; i++){
+//     for(j=1; j<=starPrint-i; j++){
+//         starString = starString+"*"
+//     }
+//     starString = starString+"\n"
+// }
+
+// console.log(starString)
+
+
+
+
+// let height = prompt("height Short/tall:- ")
+
+// if(height<1){
+//     let squeak = prompt("Can Squeak yes/no :- ")
+//     if(squeak=="yes"){
+//         console.log("Might be a rat")
+//     }else{
+//         console.log("Might be a squirrel")
+//     }
+// }
+// else if(height>=1){
+//     let neck = prompt("Short/Long neck :- ")
+//     if(neck=="short"){
+//         let nose = prompt("Short/Long nose:- ")
+//         if(nose=="short"){
+//             let stay = prompt("ON land/water:- ")
+//             if(stay=="land"){
+//                 console.log("Might be a rhinoceros")
+//             }else{
+//                 console.log("Might be a hippo")
+//             }
+//         }else{
+//             console.log("might be a elephant")
+//         }
+
+//     }else{
+//         console.log("Migh be a giraffe")
+//     }
+// }
+
+// let start = 10
+
+// for(let i =5; i>15; i--){
+//     console.log(i)
+// }
+
+
+// let currentDAte = new Date()
+
+// // console.log(currentDAte)
+// let todayDate = currentDAte.getDate()
+// console.log(todayDate)
+// let month =  currentDAte.getMonth()
+// console.log(month+1)
+// let year = currentDAte.getFullYear()
+// console.log(year)
+// let hours = currentDAte.getHours()
+// console.log(hours)
+// let min = currentDAte.getMinutes()
+// console.log(min)
+// let sec = currentDAte.getSeconds()
+// console.log(sec)
+
+// console.log(`${todayDate}/${month}/${year} ${hours}:${min}:${sec}`)
+
+// let a = 6
+// let b = 9
+// console.log(a==b)
+
+// for(i=1; i<=10; i++){
+//     console.log(i)
+// }
+
+// let i =1
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+
+
+// const arrry = ["Devanshu",89]
+
+
+// for(let aadil in arrry ){
+//     console.log(arrry[aadil])
+// }
+
+// let name = "Devanshu"
+// for(let keys in name){
+//     console.log(name[keys])
+// }
+
+// for(i = 1; i<10; i++){
+//     if(i==3){
+//         continue
+//     }
+//     console.log(i)
+// }
+
+
+
+// let i =1;
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+
+// let i = 1;
+// while(i<=20){
+//     if(i%2==0){
+//         console.log(i)
+//     }
+//     i++
+// }
+
+// let i =1
+// while(i<=100){
+//     if(i%11==0){
+//         console.log(i)
+//     }
+//     i++
+// }
+
+// let num = prompt("Enter Number:- ")
+// let i =1
+
+// while(i<=10){
+//     console.log(i*num)
+//     i++
+// }
+
+// console.log("\u20B9")
+
+// let item = prompt("Enter item :- ")
+// let price = prompt("Enter Price :- ")
+// let quantity = prompt("Enter item Quantity :- ")
+// let discount = 10
+// let gst = 20
+
+// let bill = price*quantity
+// let bill_Discount = bill*(discount/100)
+// let total_Bill = bill-bill_Discount
+// let bill_Gst = total_Bill*(gst/100)
+// let final_Bill = total_Bill+bill_Gst
+
+
+// console.log("Item :- " +item)
+// console.log("Price :- "+ price)
+// console.log("Quantity :- "+ quantity)
+// console.log("Bill :- "+bill)
+// console.log("Bill Discount :- "+bill_Discount)
+// console.log("Total Bill :- "+total_Bill)
+// console.log("Gst : - "+bill_Gst)
+// console.log("Final Bill :- "+final_Bill)
+// console.log("Yoh have to pay "+final_Bill+"\u20B9"+ " for "+quantity+"-"+item)
+
+
+// let a = 1
+
+// while(a<=10){
+//     console.log(a)
+//     a++
+// }
+
+
+// let x =1
+
+// do{
+// console.log(x)
+// x++
+// }
+// while(x<=10)
+
+// let x = ["Devanshu",98,78,"Dev"]
+
+// let output = ""
+
+// for(let key of x){
+//     output = output+key+"\n"
+// }
+// console.log(output)
+
+// let job_type = prompt("Enter job-type Government/Business/Private:- ")
+
+// if(job_type=="government"){
+//     let salary = prompt("Enter Your Monthly Salary :- ")
+//     if(salary >=50000){
+//         console.log("You can apply for loan")
+//     }
+//     else{
+//         console.log("You can Not apply for loan")
+//     }
+// }
+// else if(job_type=="business"){
+//     let salary = prompt("Enter Your Yearly turnover :- ")
+//     if(salary>2000000){
+//         console.log("You can apply for loan")
+//     }
+//     else{
+//         console.log("You can not apply for loan")
+//     }
+
+// }
+// else if(job_type=="private"){
+//     console.log("You can Not apply for loan")
+// }
+// else{
+//     console.log("Invalid Input")
+// }
+
+// const prompt = require("prompt-sync")()
+
+// const userInput = prompt("Enter Number:- ")
+
+// let userInput = 15
+
+// if(userInput<=10){
+//     for(let x =1; x<=userInput; x++){
+//         console.log("hy"+"Number-: "+x)
+//     }
+// }
+// else if(userInput<=20){
+//     for(let x =1; x<=userInput; x++){
+//         console.log("by" +"Number-: "+x)
+//     }
+// }
+
+// let num = prompt("Enter Number :- ")
+
+// //4
+
+// if(num<=10){
+//     for(let x=1; x<=num; x++){
+//         console.log("Hy"+" "+x)
+//     }
+// } else if(num<=20){
+//     for(let x =1; x<=num; x++){
+//         console.log("By"+" "+x)
+//     }
+// }
+
+
+//let const = block
+
+//var = global
+
+// var z = 5
+
+// if(true){
+//     // let x = 2;
+//     // const y = 5;
+//     var z = 6;
+// }
+
+
+// console.log(x)
+// console.log(y);
+// console.log(z);
+
+
+// function myfn(a,b){
+//      console.log(a+b)
+// }
+
+
+// myfn(8,9)
+
+
+// let a = 10
+
+// var b = 17
+
+// {
+//     let a = 19
+//     var b = 198
+// }
+
+// console.log(a)
+// console.log(b);
+
+
+/*
+
+*****
+*****
+*****
+*****
+*****
+
+
+*/
+
+
+// function myFn(){
+//     let a = 7
+//     console.log(a)
+// }
+
+// // 1sec =1000milisec
+
+// // setTimeout(myFn,5000)
+
+// setInterval(myFn,2000)
+
+let star = 5
+let myString = ""
+
+for(let x =1; x<=star; x++){
+    for(let y =1; y<=star; y++){
+        myString = myString+"+"
+    }
+    myString = myString+"\n"
+}
+
+console.log(myString)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
